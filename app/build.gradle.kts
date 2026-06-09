5bf504982f4abccd13d3be69add467ec6bc59cb4
xiaoyebaba <xiaoyebaba@users.noreply.github.com>
2026-06-09 19:55:35 +0800

    chore: bump to 0.11.0, fix gradle network timeout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.richardyap.novelreader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.10.0"
+        versionCode = 23
+        versionName = "0.11.0"
     }
 
     buildTypes {
